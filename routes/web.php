513cc531fd89839e8ac6c5b01d04dc6ae9da2346
Alisher1504 <ruzmetov150499@gmail.com>
2022-11-26 11:44:19 +0500

    asdmin dashboard successfully

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,9 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::group(['middleware' => ['auth', 'is_admin']], function() {
+    Route::get('/dashboard', function() {
+        return 'this is admin';
+    });
+});
